634490c97eeef3347fad5c2640043312dbf62699
Ivy Ye <ivy050111@gmail.com>
2024-07-12 11:42:37 -0500

    paypal integration

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import dotenv from 'dotenv';
+import express from "express";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import dotenv from "dotenv";
 dotenv.config();
-import connectDB from './config/db.js';
-import cookieParser from 'cookie-parser';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5001;
 
 connectDB(); // Connect to MongoDB
@@ -20,13 +20,16 @@ app.use(express.urlencoded({ extended: true }));
 // Middleware to parse cookies
 app.use(cookieParser());
 
-app.get('/', (req, res) => { 
-    res.send('Server is ready');
-})
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/orders', orderRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
 
 app.use(notFound);
 app.use(errorHandler);
--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -1,8 +1,16 @@
 import { Link, useParams } from "react-router-dom";
 import { Row, Col, ListGroup, Image, Card, Button } from "react-bootstrap";
+import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
-import { useGetOrderDetailsQuery } from "../slices/ordersApiSlice";
+import {
+  useGetOrderDetailsQuery,
+  usePayOrderMutation,
+  useGetPayPalClientIdQuery,
+} from "../slices/ordersApiSlice";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 const OrderScreen = () => {
   const { id: orderId } = useParams();
@@ -12,6 +20,38 @@ const OrderScreen = () => {
     isLoading,
     error,
   } = useGetOrderDetailsQuery(orderId);
+
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+
+  const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+
+  const {
+    data: paypal,
+    isLoading: loadingPayPal,
+    error: errorPayPal,
+  } = useGetPayPalClientIdQuery();
+
+  const { userInfo } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!errorPayPal && !loadingPayPal && paypal.clientId) {
+      const loadPayPalScript = async () => {
+        paypalDispatch({
+          type: "resetOptions",
+          value: {
+            "client-id": paypal.clientId,
+            currency: "USD",
+          },
+        });
+        paypalDispatch({ type: "setLoadingStatus", value: "pending" });
+      };
+      if (order && !order.isPaid) {
+        if (!window.paypal) {
+          loadPayPalScript();
+        }
+      }
+    }
+  }, [order, paypal, errorPayPal, loadingPayPal, paypalDispatch]);
 
   return isLoading ? (
     <Loader />
@@ -59,58 +99,58 @@ const OrderScreen = () => {
             </ListGroup.Item>
 
             <ListGroup.Item>
-                <h2>Order Items</h2>
-                { order.orderItems.map((item, index) => (
-                    <ListGroup.Item key={index}>
-                        <Row>
-                            <Col md={1}>
-                                <Image src={item.image} alt={item.name} fluid rounded />
-                            </Col>
-                            <Col>
-                                <Link to={`/product/${item.product}`}>{item.name}</Link>
-                            </Col>
-                            <Col md={4}>
-                                {item.qty} x ${item.price} = ${item.qty * item.price}
-                            </Col>
-                        </Row>
-                    </ListGroup.Item>
-                ))}
+              <h2>Order Items</h2>
+              {order.orderItems.map((item, index) => (
+                <ListGroup.Item key={index}>
+                  <Row>
+                    <Col md={1}>
+                      <Image src={item.image} alt={item.name} fluid rounded />
+                    </Col>
+                    <Col>
+                      <Link to={`/product/${item.product}`}>{item.name}</Link>
+                    </Col>
+                    <Col md={4}>
+                      {item.qty} x ${item.price} = ${item.qty * item.price}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+              ))}
             </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={4}>
-            <Card>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>
-                        <h2>Order Summary</h2>
-                    </ListGroup.Item>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h2>Order Summary</h2>
+              </ListGroup.Item>
 
-                    <ListGroup.Item>
-                        <Row>
-                            <Col>Items</Col>
-                            <Col>${order.itemsPrice}</Col>
-                        </Row>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>${order.itemsPrice}</Col>
+                </Row>
 
-                        <Row>
-                            <Col>Shipping</Col>
-                            <Col>${order.shippingPrice}</Col>
-                        </Row>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>${order.shippingPrice}</Col>
+                </Row>
 
-                        <Row>
-                            <Col>Tax</Col>
-                            <Col>${order.taxPrice}</Col>
-                        </Row>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>${order.taxPrice}</Col>
+                </Row>
 
-                        <Row>
-                            <Col>Total</Col>
-                            <Col>${order.totalPrice}</Col>
-                        </Row>
-                    </ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>${order.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
 
-                    {/* PAY ORDER PLACEHOLDER */}
-                    {/* MARK AS DELIVERED PLACEHOLDER */}
-                </ListGroup>
-            </Card>
+              {/* PAY ORDER PLACEHOLDER */}
+              {/* MARK AS DELIVERED PLACEHOLDER */}
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </>
